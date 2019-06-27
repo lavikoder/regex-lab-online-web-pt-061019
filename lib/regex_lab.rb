7cdd921b26552fde4bbad755fc101018070d67e3
Kevin Kamanda <kevkamanda@gmail.com>
2019-06-27 13:10:24 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,10 +23,9 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  #we want to match mobile numbers (\d = 10), reagrdless of formatting non-digit characters [^/D+]
-  
-  #condition ? if_true : if_false
+  #we want to match mobile numbers (\d = 10)
   number = phone.scan(/\d/)
-    number.length == 10 ? true: false 
+  #condition ? if_true : if_false
+  number.length == 10 ? true: false 
 end
 
